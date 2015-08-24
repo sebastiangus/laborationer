38bc06c578e68a64a7671ee3fa28c31392c4fd68
John Häggerud <john.haggerud@lnu.se>
2015-08-24 12:28:18 +0200

    recreated with new branch name

---
+++ src/part-1/check-values/check-values.js
@@ -0,0 +1,21 @@
+/**
+ * Module for a simple value checker
+ *
+ * @author thajo
+ * @version 1.0.0
+ */
+
+"use strict";
+
+/**
+ * Sends back a string with a message depending on what the parameters
+ * data type.
+ * @param {*} value - The variable to exam
+ * @returns {string} - A message that tells what the data type is (according to javascript).
+ * See readme
+ */
+exports.check = function(value) {
+    var startMessage = "You send me ";
+    // your code here
+    return startMessage;
+};
