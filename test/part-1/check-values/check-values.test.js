ecbb42fa31bd061c0873902650818ec3f6a118d2
John Häggerud <john.haggerud@lnu.se>
2015-08-24 12:30:05 +0200

    Added tests

--- test/part-1/check-values/check-values.test.js
+++ test/part-1/check-values/check-values.test.js
@@ -1,11 +1,62 @@
-var hello = require("../.././hello-world");
+var vc = require("../../../src/part-1/value-checker/value-checker.js");
 var expect = require("chai").expect;
 
-describe("Test the function sayHello", function() {
+describe("Test the module", function() {
     "use strict";
 
-    it("sayHello(); should return \"Hello, World!\"", function(done) {
-        expect(hello.sayHello()).to.eql("Hello, World!");
+    it("Should return a string 'You send me an object' " +
+        "when check is called with an array", function(done) {
+        var result = vc.check([]);
+        expect(result).eql("You send me an object");
+        done();
+    });
+
+    it("Should return a string 'You send me an object' " +
+        "when check is called with an object", function(done) {
+        var result = vc.check({});
+        expect(result).eql("You send me an object");
+        done();
+    });
+
+    it("Should return a string 'You send me a primitive' " +
+        "when check is called with a number", function(done) {
+        var result = vc.check(12);
+        expect(result).eql("You send me a primitive");
+        done();
+    });
+
+    it("Should return a string 'You send me a primitive' " +
+        "when check is called with a booleean", function(done) {
+        var result = vc.check(true);
+        expect(result).eql("You send me a primitive");
+        done();
+    });
+
+    it("Should return a string 'You send me a primitive' " +
+        "when check is called with a string", function(done) {
+        var result = vc.check("this is a string");
+        expect(result).eql("You send me a primitive");
+        done();
+    });
+
+    it("Should return a string 'You send me an undefined value' " +
+        "when check is called with no parameter", function(done) {
+        var result = vc.check();
+        expect(result).eql("You send me an undefined value");
+        done();
+    });
+
+    it("Should return a string 'You send me a function' " +
+        "when check is called with a function", function(done) {
+        var result = vc.check(function() {});
+        expect(result).eql("You send me a function");
+        done();
+    });
+
+    it("Should return a string 'You send me an object' " +
+        "when check is called with null", function(done) {
+        var result = vc.check(null);
+        expect(result).eql("You send me an object");
         done();
     });
 
